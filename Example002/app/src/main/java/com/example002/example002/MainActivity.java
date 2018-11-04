0cec04cc923e9dc3e8534e0bc5895d764f7c9a55
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-04 20:05:03 +0000

    Example002

--- Example002/app/src/main/java/com/example002/example002/MainActivity.java
+++ Example002/app/src/main/java/com/example002/example002/MainActivity.java
@@ -2,6 +2,7 @@ package com.example002.example002;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "This is an information message");
+        Log.w("MainActivity", "This is a warning message");
+        Log.e("MainActivity", "This is an error message");
+        Log.d("MainActivity", "This is a debug message");
+        Log.v("MainActivity", "This is a verbose message which is reserved for really really long text");
+
+        Log.wtf("MainActivity", "This is a WTF/Assertion Failure message. If an assertion fails in your code log it with this.");
+
+
     }
 }
