5e1151138f6d1de326354ce597fe9787ff5a892d
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-04 21:29:34 +0000

    Example006

--- Example006/app/src/main/java/com/example006/MainActivity.java
+++ Example006/app/src/main/java/com/example006/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example006;
 
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources resources = getResources();
+        TypedArray typedStringArray = resources.obtainTypedArray(R.array.my_string_array);
+        TypedArray typedIntArray = resources.obtainTypedArray(R.array.int_array);
+        TypedArray typedFloatArray = resources.obtainTypedArray(R.array.float_array);
+
+        @SuppressLint("ResourceType")
+        String textViewString = typedStringArray.getString(0) + ", " +
+                typedStringArray.getString(1) + ", " + typedStringArray.getString(2);
+
+        @SuppressLint("ResourceType")
+        String textViewIntArrayString = typedIntArray.getInt(0, 0) + ", " +
+                typedIntArray.getInt(1, 0) + ", " + typedIntArray.getInt(2, 0);
+
+        @SuppressLint("ResourceType")
+        String textViewFloatArrayString = typedFloatArray.getFloat(0, 0) + ", " +
+                typedFloatArray.getFloat(1, 0) + ", " + typedFloatArray.getFloat(2, 0);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(textViewFloatArrayString);
     }
 }
