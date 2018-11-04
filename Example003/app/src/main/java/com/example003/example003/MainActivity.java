85f153a425c468961dd13f9afa22325d38f06443
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-04 20:16:16 +0000

    Example003

--- Example003/app/src/main/java/com/example003/example003/MainActivity.java
+++ Example003/app/src/main/java/com/example003/example003/MainActivity.java
@@ -2,6 +2,7 @@ package com.example003.example003;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "This is a long toast message", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "This is a short toast message", Toast.LENGTH_SHORT).show();
     }
 }
