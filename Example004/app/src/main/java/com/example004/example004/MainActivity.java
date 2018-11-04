de50b3ccaf02652a4f4a7e33710f7eb79c8452ec
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-04 20:33:38 +0000

    Example4

--- Example004/app/src/main/java/com/example004/example004/MainActivity.java
+++ Example004/app/src/main/java/com/example004/example004/MainActivity.java
@@ -2,6 +2,7 @@ package com.example004.example004;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.text_view);
+        textView.setText("This was changed from inside MainActivity.");
+        textView.setTextSize(20);
     }
 }
