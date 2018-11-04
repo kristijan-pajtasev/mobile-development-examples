2a02fae6c976f716204b65895183bcd38d7acbbd
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-04 20:55:21 +0000

    Example005

--- Example005/app/src/main/java/com/example005/example005/MainActivity.java
+++ Example005/app/src/main/java/com/example005/example005/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example005.example005;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get plural from the resources and apply it to a string for testing
+        Resources resources = getResources();
+        String textViewContent = resources.getQuantityString(R.plurals.dollar_plura, 1, 1);
+
+        // pull the textView from the layout and set the string
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(textViewContent);
     }
 }
