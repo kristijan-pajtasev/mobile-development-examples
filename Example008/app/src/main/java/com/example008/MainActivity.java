ad01289cf99a96d23f684bc7712c49aa3fb168bb
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-05 22:50:51 +0000

    Example008

--- Example008/app/src/main/java/com/example008/MainActivity.java
+++ Example008/app/src/main/java/com/example008/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example008;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources resources = getResources();
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageDrawable(resources.getDrawable(R.mipmap.ic_launcher));
     }
 }
