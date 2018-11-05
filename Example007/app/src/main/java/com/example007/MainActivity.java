61d68c653a2466d71bfac681b87bc0617ddf4afd
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-05 22:42:27 +0000

    Example007

--- Example007/app/src/main/java/com/example007/MainActivity.java
+++ Example007/app/src/main/java/com/example007/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example007;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources resources = getResources();
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setBackgroundColor(resources.getColor(R.color.myRed));
     }
 }
